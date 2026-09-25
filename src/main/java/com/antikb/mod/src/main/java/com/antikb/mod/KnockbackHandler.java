c2e8b10038cab41d47a5c83d64cd52c3137240ea
blackbear777 <taukira079@gmail.com>
2026-09-25 10:55:25 +0530

    Update KnockbackHandler.java

--- src/main/java/com/antikb/mod/src/main/java/com/antikb/mod/KnockbackHandler.java
+++ src/main/java/com/antikb/mod/src/main/java/com/antikb/mod/KnockbackHandler.java
@@ -1,19 +1,34 @@
 package com.antikb.mod;
 
-import net.minecraftforge.event.entity.living.LivingKnockBackEvent;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.IAttributeInstance;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 public class KnockbackHandler {
 
     @SubscribeEvent
-    public void onKnockback(LivingKnockBackEvent event) {
-        float multiplier = AntiKnockback.strengthMultiplier;
-
-        if (multiplier <= 0.0f) {
-            event.setStrength(0.0f);
+    public void onPlayerTick(TickEvent.PlayerTickEvent event) {
+        if (event.phase != TickEvent.Phase.START) {
+            return;
+        }
+        if (!(event.player instanceof EntityPlayer)) {
             return;
         }
 
-        event.setStrength(event.getStrength() * multiplier);
+        EntityPlayer player = event.player;
+        IAttributeInstance attribute = player.getEntityAttribute(SharedMonsterAttributes.knockbackResistance);
+        if (attribute == null) {
+            return;
+        }
+
+        double desired = 1.0 - AntiKnockback.strengthMultiplier;
+        if (desired < 0.0) desired = 0.0;
+        if (desired > 1.0) desired = 1.0;
+
+        if (attribute.getBaseValue() != desired) {
+            attribute.setBaseValue(desired);
+        }
     }
 }
